2d0c534c2d503bdda91c54b5db0d07ba47487c08
k.kaszubski <k.kaszubski@samsung.com>
2018-11-09 15:58:29 +0100

    more exercises - generators

--- sqlalchemy/queries.py
+++ sqlalchemy/queries.py
@@ -10,27 +10,32 @@ session = Session()
 movies = session.query(Movie).all()
 
 print('\n### All movies:')
-for movie in movies:
-    print(f'{movie.title} was released on {movie.release_date}')
+[print('%s was released on %s'%(movie.title, movie.release_date)) for movie in movies]
+# for movie in movies:
+#     print('%s was released on %s'%(movie.title, movie.release_date))
+    # print(f'{movie.title} was released on {movie.release_date}')
 print('')
 
 movies = session.query(Movie).filter(Movie.release_date > date(2015, 1, 1)).all()
 
 print('\n### Recent movies:')
 for movie in movies:
-    print(f'{movie.title} was released after 2015')
+    print('%s was released after 2015'%movie.title)
+    # print(f'{movie.title} was released after 2015')
 print('')
 
 the_rock_movies = session.query(Movie).join(Actor, Movie.actors).filter(Actor.name == 'Dwayne Johnson').all()
 
 print('\n### Dwayne Johnson movies:')
 for movie in the_rock_movies:
-    print(f'Dwayne Johnson was playing in {movie.title}')
+    print('Dwayne Johnson was playing in %s'%movie.title)
+    # print(f'Dwayne Johnson was playing in {movie.title}')
 print('')
 
 glendale_stars = session.query(Actor).join(ContactDetails).filter(ContactDetails.address.ilike('%glendale%')).all()
 
 print('\n### Glendale actors')
 for actor in glendale_stars:
-    print(f'{actor.name} has a house in Glendale')
+    print('%s has a house in Glendale'%actor.name)
+    # print(f'{actor.name} has a house in Glendale')
 print('')
